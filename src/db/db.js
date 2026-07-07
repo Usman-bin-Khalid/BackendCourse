e9301d2bea7cd80dd6ac9c1245c1a355e2424976
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-07-07 11:39:37 +0500

    Server, App, And Notes Model Created

---
+++ src/db/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+   await mongoose.connect('mongodb+srv://usmanbinkhalidpk_db_user:ILwjuljgNgC1d5tj@cluster0.n7bfj84.mongodb.net/halley');
+   // '/' is ky bd jo likhyn gyn wo database ka name hoga, agr database exist nhi krta to wo create ho jaye ga
+   console.log('Connected to MongoDB');
+}
+
+module.exports = connectDB;
