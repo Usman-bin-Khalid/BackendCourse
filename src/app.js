8826bdf06f95617021da9f3ed19ff9cd7867e167
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-07-13 13:05:09 +0500

    Create Post API Completed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const noteModel = require("./models/note.model");
+const postModel = require("./models/post.model");
 const uploadFile = require("./services/storage.service");
 const app = express();
 app.use(express.json());
@@ -52,7 +53,25 @@ app.post('/create-post', upload.single('image'), async (req , res) => {
   console.log(req.body);
   cosole.log(req.file);
   const result = await uploadFile(req.file.buffer);
+  const post = await postModel.create({
+    image : result.url,
+    caption : req.body.caption,
+  });
+   return res.status(201).json({
+    message : 'Post created successfully',
+    post : post
+   })
 });
+
+
+
+app.get('/posts', async (req , res) => {
+     const posts = await postModel.find();
+     return res.status(200).json({
+      message : 'Post fetched successfully',
+      posts : posts,
+     })
+})
 
 
 module.exports = app;
