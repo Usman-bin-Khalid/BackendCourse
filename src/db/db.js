6a5735a10de154ba3241aca9fb0499f9821c08cd
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-07-14 15:13:42 +0500

    User Model Created

--- src/db/db.js
+++ src/db/db.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 async function connectDB() {
-   await mongoose.connect(process.env.MONGO_URI, );
+   try {
+       await mongoose.connect(process.env.MONGO_URI, );
    // '/' is ky bd jo likhyn gyn wo database ka name hoga, agr database exist nhi krta to wo create ho jaye ga
    console.log('Connected to MongoDB');
+   } catch (error) {
+       console.error('Error connecting to MongoDB:', error);
+   }
 }
 
 
